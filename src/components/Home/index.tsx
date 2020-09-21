6d227d4c8996610c4490e633b1e9e42bef4ecfd5
lizuoxing <lizuoxing4534@gmail.com>
2020-09-21 20:25:18 +0800

    init project

---
+++ src/components/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Products from '../Products';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>home</h1>
+      <Products />
+    </div>
+  )
+}
+
+export default Home;
---
+++ src/components/Products/index.tsx
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react';
+import { AppContext } from '../../context';
+import { Types } from '../../reducers';
+
+const Products = () => {
+  const { state, dispatch } = useContext(AppContext);
+
+  return (
+    <div>
+      <button onClick={() => {
+        dispatch({
+          type: Types.Add,
+        })
+      }}>
+        click
+        </button>
+      {state.shoppingCart}
+    </div>
+  )
+}
+
+export default Products;
